9f120c5e2234a8e5a746ae12e489bc32786b72c7
olyabainova-lgtm <olyabainova-lgtm@users.noreply.github.com>
2026-07-03 11:07:42 +0500

    Настроить публикацию GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 import './theme.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>,
+  <React.StrictMode><HashRouter><App /></HashRouter></React.StrictMode>,
 )
